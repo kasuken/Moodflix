8061bb35316c866cb4c478c22b6cfac8a24a6897
Davide Mandelli <mandellidavide.95@gmail.com>
2022-02-16 02:23:40 +0100

    Added logo and fixed README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Moodflix
+![Image of Moodflix Project](https://cdn.jsdelivr.net/gh/Th3Wall/assets-cdn/Moodflix/Moodflix_Backdrop_Placeholder.png)
+
+## Moodflix
 Our project for The Microsoft Azure Trial Hackathon on Dev.to
--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,9 @@
+const GITHUB_ASSETS_BASE_URL = "https://cdn.jsdelivr.net/gh/Th3Wall/assets-cdn/Moodflix"
 export const ENDPOINT_BASE_URL = "https://moodflix.azurewebsites.net";
 export const BASE_IMG_URL = "https://image.tmdb.org/t/p/original";
-export const FALLBACK_POSTER_IMG_URL = "https://cdn.jsdelivr.net/gh/Th3Wall/assets-cdn/Moodflix/Moodflix_Poster_Placeholder.png";
-export const FALLBACK_BACKDROP_IMG_URL = "https://cdn.jsdelivr.net/gh/Th3Wall/assets-cdn/Moodflix/Moodflix_Backdrop_Placeholder.png";
+export const FALLBACK_POSTER_IMG_URL = `${GITHUB_ASSETS_BASE_URL}/Moodflix_Poster_Placeholder.png`;
+export const FALLBACK_BACKDROP_IMG_URL = `${GITHUB_ASSETS_BASE_URL}/Moodflix_Backdrop_Placeholder.png`;
+export const LOGO_URL = `${GITHUB_ASSETS_BASE_URL}/Moodflix_logo.png`;
 export const LANG = "en-US";
 const { REACT_APP_TMDB_API_KEY } = process.env;
 
