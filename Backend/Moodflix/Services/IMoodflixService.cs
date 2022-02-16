f51e1ea822a86fa00223396643141e03c479d12e
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-02-16 22:11:01 +0100

    sentiment and refactoring

--- Backend/Moodflix/Services/IMoodflixService.cs
+++ Backend/Moodflix/Services/IMoodflixService.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using Moodflix.Models;
 using TMDbLib.Objects.Search;
 
 namespace Moodflix.Services
 {
     public interface IMoodflixService
     {
-
-        public Task<DetectedFace> AnalyseSentimentFromPhoto(string base64photo);
-        public Task<List<SearchTv>> RetrieveMoviesBySentiment(string emotion);
-
+        Task<DetectedFace> AnalyseSentimentFromPhoto(string base64photo);
+        Task<MoviesBySentimentResponse> RetrieveMoviesBySentiment(string emotion);
+        Task<MovieResponse> RetrieveMoviesById(string id);
     }
 }
