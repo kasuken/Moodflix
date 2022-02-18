034a86d05442f70915f76adca7729c52e2376db7
Davide Mandelli <mandellidavide.95@gmail.com>
2022-02-18 03:00:56 +0100

    Added slide in transition and react-icons pkg

--- Frontend/src/components/Modal/Modal.jsx
+++ Frontend/src/components/Modal/Modal.jsx
@@ -1,7 +1,19 @@
 import "./modal.scss";
-import {AnimatePresence, motion} from "framer-motion";
-import {useModalValue} from "../../context/ModalProvider";
 import {useRef} from "react";
+import {useModalValue} from "../../context/ModalProvider";
+import {AnimatePresence, motion} from "framer-motion";
+import { VscChromeClose } from "react-icons/vsc";
+import {defaultEasing} from "../../motionUtils";
+
+export const modalVariants = {
+  hidden: { opacity: 0, top: "100%", transition: { duration: .6, ease: defaultEasing } },
+  visible: { opacity: 1, top: "50%", transition: { duration: .8, ease: defaultEasing } }
+}
+
+export const modalOverlayVariants = {
+  hidden: { opacity: 0, transition: { duration: .2, delay: .5 } },
+  visible: { opacity: 1, transition: { duration: .2 } }
+}
 
 const Modal = () => {
   const modalRef = useRef(null);
@@ -12,18 +24,24 @@ const Modal = () => {
     <AnimatePresence exitBeforeEnter>
       {isModalVisible && (
         <>
-          {/*Overlay*/}
-          <motion.div key="modalOverlay" className={`modal__overlay ${!isModalVisible && 'modal__invisible'}`}>
-
-            {/*Content*/}
-            <motion.div key="modal" ref={modalRef} className={`modal__wrp ${!isModalVisible && 'modal__invisible'}`}>
-
+          <motion.div
+            key="modalOverlay"
+            className={`modal__overlay ${!isModalVisible && 'modal__invisible'}`}
+            variants={modalOverlayVariants}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          >
+            <motion.div
+              key="modal"
+              ref={modalRef}
+              className={`modal__wrp ${!isModalVisible && 'modal__invisible'}`}
+              variants={modalVariants}
+            >
               <motion.button className="modal__closebtn" onClick={handleModalClose}>
-                {/*<VscChromeClose />*/}
+                <VscChromeClose />
               </motion.button>
-
             </motion.div>
-
           </motion.div>
         </>
       )}
